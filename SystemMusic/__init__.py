f1418afac89a631a8290a88ed2b9876f5f6b062a
𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛𝐒𝚈ѕ𝐓𝚄M ٭》[🇮🇳] <ownersystum@gmail.com>
2025-07-25 11:28:38 +0530

    Update __init__.py

--- SystemMusic/__init__.py
+++ SystemMusic/__init__.py
@@ -27,4 +27,4 @@ Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
 
-APP = "ll_DRAGON_MUSIC_BOT"  # connect music api key "Dont change it"
+APP = "RadhaaMusicBot"  # connect music api key "Dont change it"
